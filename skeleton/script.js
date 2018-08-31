0785e3c602ae42dd47538ee5470e8125b7f1734e
Joey Feng <jfeng702@gmail.com>
2018-08-30 19:14:25 -0700

    update

--- skeleton/script.js
+++ skeleton/script.js
@@ -18,7 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
-  // --- your code here!
   const input = document.querySelector('.favorite-input');
   const div = document.querySelector('.list-container');
   div.addEventListener('submit', (e) => {
@@ -33,8 +32,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   // adding new photos
+  const button = document.querySelector('.photo-show-button');
+  button.addEventListener('click', (e) => {
+    const photoForm = document.querySelector('.photo-form-container');
+    photoForm.classList.toggle('hidden');
 
-  // --- your code here!
+  });
+
+  const photoSubmit = document.querySelector('.photo-url-submit');
+  photoSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const photoInput = document.querySelector('.photo-url-input');
+    const img = document.createElement('img');
+    img.src = photoInput.value;
+    const photoUl = document.querySelector('.dog-photos');
+    const photoLi = document.createElement('li');
+    photoLi.appendChild(img);
+    photoUl.appendChild(photoLi);
+    photoInput.value = '';
+  });
+
 
 
 
